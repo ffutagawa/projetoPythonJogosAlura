e7d42d7ece03417fbaf4a6095297a0208fbdab23
FFutagawa <ffutagawa@gmail.com>
2025-03-24 21:55:59 -0300

    Implementando regras do jogo

--- forca.py
+++ forca.py
@@ -3,20 +3,27 @@ def jogar():
     print("**JOGO DE FORCA**")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_",]
+
     enforcou  = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while (not enforcou and not acertou):
         chute = input("Informe uma letra: ")
         chute = chute.strip()
 
-
         index = 0
         ##for letra in palavra_secreta:
-        for index, letra in enumerate(palavra_secreta):
+        for letra in palavra_secreta:
             if(chute.lower() == letra.lower()):
-                print(f"Encontrei a letra {letra} na posição {index + 1}")
+                letras_acertadas [index] = letra
             index = index + 1
+
+        print(letras_acertadas)
+
         print("jogando..")   
 
 if(__name__ == "__main__"):
